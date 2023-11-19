4048a43a90bdd7857043a8027e681960c464ef58
SaitoMikie <mikie.saito@spectruminc.io>
2023-11-19 19:45:05 +0900

    Todoの一覧を呼び出すapi予定地

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express, { Request, Express, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+// Todoの一覧を呼び出すapi
+app.get("/allTodos", (req: Request, res: Response) => {
+  return res.send("Todos");
+});
+
+app.listen(PORT, () => console.log("server is running"));
