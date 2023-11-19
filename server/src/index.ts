b68973a4f590c0389e2c61555d29367133fc81ab
SaitoMikie <mikie.saito@spectruminc.io>
2023-11-19 20:10:22 +0900

    Todoの一覧を取得するapi作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Express, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
 const PORT = 8080;
 
+const prisma = new PrismaClient();
+
 // Todoの一覧を呼び出すapi
-app.get("/allTodos", (req: Request, res: Response) => {
-  return res.send("Todos");
+app.get("/allTodos", async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany();
+  return res.json(allTodos);
 });
 
 app.listen(PORT, () => console.log("server is running"));
